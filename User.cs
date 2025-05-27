bddeb593f2eadf5e4dba26f3461f4303c36629f7
Mehmet Birol Gölge <golgemehmetbirol@gmail.com>
2025-05-27 12:33:54 +0300

    Models klasoru ve ilgili Class dosylari, app config dosyasi, TodoContext sinifinin tamamlanmasi

---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MehmetBirolGolge_231502010.Models
+{
+	public class User
+	{
+		[Key]
+		public int UserID { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Username { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Password { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Email { get; set; }
+
+		[StringLength(20)]
+		public string Role { get; set; } = "User"; // Default olarak User
+
+		public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+		// Navigation Property
+		public virtual ICollection<Task> Tasks { get; set; }
+	}
+}
